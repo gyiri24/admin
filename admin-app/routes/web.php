8b228ca28430e6b14308f8ca2a29855fdc0af7cd
gyoridanidavid <gyiri24@gmail.com>
2022-10-16 18:44:38 +0200

    create db seeder

--- admin-app/routes/web.php
+++ admin-app/routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [EmployeeController::class, 'index'])->name('home');
+
+Route::get('/about', function() {
+    return view('about');
+});
