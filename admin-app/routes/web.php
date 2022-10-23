e1befa41f9e58faa9ec9db94f00f3ff84ca07df3
gyoridanidavid <gyiri24@gmail.com>
2022-10-23 15:33:44 +0200

    remove laravel logo from login page

--- admin-app/routes/web.php
+++ admin-app/routes/web.php
@@ -19,17 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    });
-
-    Route::resource('employees', EmployeeController::class)
-        ->middleware('is_admin');
-});
-
-require __DIR__.'/auth.php';
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::resource('employees', EmployeeController::class)->middleware('auth');
